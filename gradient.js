71229adfa0fcc0d5fed4cc9ca8d198ff7d197464
Samantha Heckle <t-saheck@microsoft.com>
2017-06-29 15:45:29 -0700

    finalized gradient generation

---
+++ gradient.js
@@ -0,0 +1,36 @@
+// JavaScript source code
+
+// create global variable to store the random colors
+var colors;
+
+// function to call the JSON file
+$(document).ready(function () {
+
+    // calling the canvas element
+    var c = document.getElementById("canvas");
+
+    // setting the width and height to the screen
+    document.getElementById("container").height = window.innerWidth;
+    document.getElementById("container").width = window.innerHeight;
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+
+    // getting the context
+    var ctx = c.getContext("2d");
+
+    // creating a horizontal gradient according to device height
+    var my_gradient = ctx.createLinearGradient(0, 0, 0, c.height);
+
+    // jQuery call for JSON file
+    $.getJSON('media/gradients.json', function (json) {
+        // setting color variable to random color generator
+        colors = (json[Math.floor((Math.random() * json.length))].colors);
+        // adds the colors to the gradient on the canvas
+        my_gradient.addColorStop(0, colors[0]);
+        my_gradient.addColorStop(1, colors[1]);
+        // fills the gradient
+        ctx.fillStyle = my_gradient;
+        ctx.fillRect(0, 0, c.width, c.height);
+    });
+    //console.log(colors);
+});
